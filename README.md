6eebc0e493ac4bf2b4a2cb1d4a1a4099c48288c3
Nat Epstein <nathaniel.epstein@gmail.com>
2021-11-13 15:31:26 -0500

    added comments to html and JS files to explain how code works

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Nat-Epstein-Day-Planner
+
+
+
+
+
+
+
+Sources: https://fontawesome.com/v5.15/icons/save?style=solid (this was so I could use the save icon in font awesome)
+
+Asked tutor and ASKBCS for help with local storage
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,55 @@
 function setTime() {
-  // We want to grab the div id of currentDay and then display it right under the description of the app. We can acheive this using jQuery to select the html div id and then using the moment library to capture what day it is
-  $("#currentDay").text(moment().format('MMMM Do YYYY'));
-  //Below is the alternate way of doing this using regular Web APIs
-  //var currentDay = document.getElementById('currentDay');
-  //currentDay.textContent = moment().format('MMMM Do YYYY');
+    // We want to grab the div id of currentDay and then display it right under the description of the app. We can acheive this using jQuery to select the html div id and then using the moment library to capture what day it is
+    //$("#currentDay").text(moment().format('MMMM Do YYYY'));-this is the jQuery method to access the currentDay id and then display it on screen
+    let currentDay = document.getElementById('currentDay');
+    currentDay.textContent = moment().format('MMMM Do YYYY');
 
-  // We are calling the function changeTimeBlocksColor (this function allows the program to change each of the time block colors depending on whether the time block is in the past, present or future relative to the current time). We also want the program to be able to update the blocks every minute so we must set the interval to 60000 miliseconds, which is equivalent to 1 minute
-  changeTimeBlocksColor();
-  setInterval(changeTimeBlocksColor, 60000);
+    // We are calling the function changeTimeBlocksColor (this function allows the program to change each of the time block colors depending on whether the time block is in the past, present or future relative to the current time). We also want the program to be able to update the blocks every minute so we must set the interval to 60000 miliseconds, which is equivalent to 1 minute
+    changeTimeBlocksColor();
+    setInterval(changeTimeBlocksColor, 60000);
 
-  // This loops through the time-block class
-  $(".time-block").each(function() {
-    var blockId = $(this).attr("id");
-    // This line of code allows us to retrieve any of the stored data within the text box
-    $("#" + blockId + " textarea").text(localStorage.getItem(moment().format("MMMM Do YYYY") + blockId));
-  });
+    // This loops through the time-block class
+    $(".time-block").each(function () {
+        let blockId = $(this).attr("id");
+        //console.log(blockId);
+        // This line of code allows us to retrieve any of the stored data within the text box for each hour
+        $("#" + blockId + " textarea").text(localStorage.getItem(moment().format("MMMM Do YYYY") + blockId));
+    });
 
-  // This is the event listener when we click the save button
-  $(".saveBtn").on("click", handleSave);
+    // This is the event listener when we click the save button
+    $(".saveBtn").on("click", clickSave);
 }
 // Calling the setTime function so that the code will execute 
 setTime();
 
-function changeTimeBlocksColor() {
-  // This line of code allows us to loop through the time-block classes 
-  $(".time-block").each(function() {
-    var blockHour = parseInt($(this).attr("id").replace("hour-", ""));
-    var currentHour = parseInt(moment().format("H"));
-    // We need to remove the classes from the time blocks so that they can change according to the current time
-    $(this).removeClass("past present future");
-    // We need these conditional statements to know what each time block color is going to be. Without it the 
-    if (blockHour < currentHour) {
-      $(this).addClass("past");
-    } else if (blockHour > currentHour) {
-      $(this).addClass("future");
-    } else {
-      $(this).addClass("present");
-    }
-  });
-}
+// This will allow the save button, when clicked, to store data to local storage
+function clickSave(event) {
+    let hourId = $(this).parent().attr("id");
+    localStorage.setItem(moment().format("MMMM Do YYYY") + hourId, $("#" + hourId + " textarea").val());
+};
 
-function handleSave(event) {
-  var hourId = $(this).parent().attr("id");
-  localStorage.setItem(moment().format("MMMM Do YYYY") + hourId, $("#" + hourId + " textarea").val());
-}
+function changeTimeBlocksColor() {
+    // This line of code allows us to loop through the time-block classes 
+    $(".time-block").each(function () {
+        let blockHour = parseInt($(this).attr("id").replace("hour-", ""));
+        //console.log(blockHour);
+        //We only want the number (integer) to be parsed through so we have to get make sure that any letters or characters that aren't intergers are not included
+        let currentHour = parseInt(moment().format("H"));
+        //console.log(currentHour);
+        // We need to remove the classes from the time blocks so that they can change according to the current time
+        $(this).removeClass("past present future");
+        // We need these conditional statements to know what each time block color is going to be
+        // for all future hours relative to current hour the block will appear green
+        if (blockHour > currentHour) {
+            $(this).addClass("future");
+            // for all future hours relative to the current hour the block will appear green
+        } else if (blockHour < currentHour) {
+            $(this).addClass("past");
+            // Otherwise it means the hour is present and the block will appear red
+        } else {
+            $(this).addClass("present");
+        }
+    });
+};
+
 
